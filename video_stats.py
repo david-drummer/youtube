a67e9fa50a89f5f948ea99a3da0ea63562c73faf
Dobos David <dobosdavid.mail@gmail.com>
2026-03-08 13:32:16 +0100

    Add function to retrieve all video IDs

--- video_stats.py
+++ video_stats.py
@@ -7,10 +7,8 @@ load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
 
-#API_KEY = 'AIzaSyBNHUiSjnOoI6GwPxHiye_dKLLOctB-uXw'
-
 CHANNEL_HANDLE = 'VivaLaDirtLeague'
-
+maxResults = 50
 url = f'https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}'
 
 def get_playlistId():
@@ -18,7 +16,6 @@ def get_playlistId():
         response = requests.get(url)
 
         response.raise_for_status()
-        print(response)
 
         data = response.json()
 
@@ -28,12 +25,48 @@ def get_playlistId():
 
         channel_playlistId = channel_items['contentDetails']['relatedPlaylists']['uploads']
 
-        print(channel_playlistId)
 
         return channel_playlistId
     except requests.exceptions.RequestException as e:
         raise e
     
+def get_video_ids(playlistId):
+
+    video_ids = []
+    
+    pageToken = None
+
+    base_url = f'https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={playlistId}&key={API_KEY}'
+
+    try:
+        while True:
+
+            url = base_url
+
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+
+            response = requests.get(url)
+
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get('items', []):
+                video_id = item['contentDetails']['videoId']
+                video_ids.append(video_id)
+
+            pageToken = data.get('nextPageToken')
+
+            if not pageToken:
+                break
+
+        return video_ids
+        
+    except requests.exceptions.RequestException as e:
+        raise e
+
 if __name__ == "__main__":
-    get_playlistId()
+    playlistId = get_playlistId()
+    print(get_video_ids(playlistId))
 
